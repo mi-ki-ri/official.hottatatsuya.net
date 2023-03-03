c2e69655d2b234341cee78484c2c2714c0f61194
HOTTA Tatsuya <hotta.tatsuya.fcc@gmail.com>
2023-03-03 17:18:47 +0900

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://mi-ki-ri.github.io/official.hottatatsuya.net/",
+  base: "/official.hottatatsuya.net/",
 });
